55efdd4da67c907846d4e584ccdd8686a0edb062
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-12-11 22:47:44 -0500

    header

--- Apple/src/components/header.jsx
+++ Apple/src/components/header.jsx
@@ -1,8 +1,27 @@
 export default function Header() {
   return (
     <>
-      <header className="fixed w-full">
-        <nav className=""></nav>
+      <header className="flex flex-row justify-center items-center gap-6 font-thin fixed w-full pt-4 ">
+        <div className="">
+          <i className="ri-apple-fill"></i>
+        </div>
+        <nav className="">
+          <ul className="flex text-xs gap-6">
+            <li>Mac</li>
+            <li>iPad</li>
+            <li>iPhone</li>
+            <li>Watch</li>
+            <li>AirPods</li>
+            <li>TV & Home</li>
+            <li>Entertainment</li>
+            <li>Accessories</li>
+            <li>Support</li>
+          </ul>
+        </nav>
+        <div className=" flex flex-row justify-center gap-6">
+          <i className="ri-search-line"></i>
+          <i className="ri-shopping-bag-line"></i>
+        </div>
       </header>
     </>
   );
