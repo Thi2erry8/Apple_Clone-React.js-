c888ef99e448dc8b406051be16f5a565f2799841
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-12-15 00:45:12 -0500

    Framer-Motion Test

--- Apple/src/pages/home.jsx
+++ Apple/src/pages/home.jsx
@@ -1,10 +1,24 @@
 import Header from "../components/header";
+import iphone from "../assets/images/iphone_hero.jpg";
+import { motion } from "framer-motion";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <section className="h-screen w-full"></section>
+      <section className="h-screen w-full flex flex-col justify-center items-center ">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }}
+          className=""
+        >
+          <p>Iphone 17 pro</p>
+        </motion.div>
+        <div className="">
+          <img src={iphone} width="500px" alt="" />
+        </div>
+      </section>
     </>
   );
 }
