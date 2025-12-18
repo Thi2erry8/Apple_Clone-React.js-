32522e8c092a30fd5461d71c192504a432228fcb
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-12-17 20:39:57 -0500

    01110001

---
+++ Apple/src/components/hero.jsx
@@ -0,0 +1,22 @@
+import iphone from "../assets/images/iphone_hero.jpg";
+import { motion } from "framer-motion";
+
+export default function hero() {
+  return (
+    <>
+      <section className="h-screen w-full flex flex-col justify-center items-center ">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ delay: 1.5 }}
+          className=""
+        >
+          <p>Iphone 17 pro</p>
+        </motion.div>
+        <div className="">
+          <img src={iphone} width="500px" alt="" />
+        </div>
+      </section>
+    </>
+  );
+}
--- Apple/src/pages/home.jsx
+++ Apple/src/pages/home.jsx
@@ -1,24 +1,11 @@
 import Header from "../components/header";
-import iphone from "../assets/images/iphone_hero.jpg";
-import { motion } from "framer-motion";
+import Hero from "../components/hero";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <section className="h-screen w-full flex flex-col justify-center items-center ">
-        <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 1.5 }}
-          className=""
-        >
-          <p>Iphone 17 pro</p>
-        </motion.div>
-        <div className="">
-          <img src={iphone} width="500px" alt="" />
-        </div>
-      </section>
+      <Hero />
     </>
   );
 }
